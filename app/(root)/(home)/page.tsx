d42eb950e90f00e8ca10e142514e226e54223f08
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-02-21 00:46:27 +0530

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">sdsfsfs</h1>
+    <div className="h-screen">
+      <UserButton />
     </div>
   );
 }
