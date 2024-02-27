fbf89c4b61ec45b3132ea7b074503e7ed9b02dc2
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-02-27 19:50:58 +0530

    adding No Result page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,54 @@
 import HomeFilters from '@/components/home/HomeFilters';
 import Filter from '@/components/shared/Filter';
+import NoResult from '@/components/shared/NoResult';
 import LocalSearch from '@/components/shared/search/LocalSearch';
 import { Button } from '@/components/ui/button';
 import { HomePageFilters } from '@/constants/filters';
 import Link from 'next/link';
 
+const questions = [
+  {
+    _id: 1,
+    title: 'Cascading Deletes in SQLAlchemy',
+    tags: [
+      { _id: 1, name: 'python' },
+      { _id: 2, name: 'javascript' },
+      { _id: 3, name: 'html' },
+    ],
+    author: 'John Doe',
+    upvotes: 10,
+    views: 100,
+    answers: 2,
+    createdAt: '2023-02-27T19:00:00Z',
+  },
+  {
+    _id: 2,
+    title: 'What is HTML',
+    tags: [
+      { _id: 1, name: 'python' },
+      { _id: 2, name: 'javascript' },
+      { _id: 3, name: 'html' },
+    ],
+    author: 'John Doe',
+    upvotes: 8,
+    views: 10,
+    answers: 5,
+    createdAt: '2023-02-27T19:00:00Z',
+  },
+  {
+    _id: 3,
+    title: 'What is CSS',
+    tags: [
+      { _id: 1, name: 'javascript' },
+      { _id: 2, name: 'html' },
+    ],
+    author: 'John Doe',
+    upvotes: 10,
+    views: 100,
+    answers: 2,
+    createdAt: '2023-02-27T19:00:00Z',
+  },
+];
 export default function Home() {
   return (
     <>
@@ -28,12 +72,25 @@ export default function Home() {
 
         <Filter
           filters={HomePageFilters}
-          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          otherClasses="m in-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
         />
       </div>
 
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => 'QuestionCard')
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
