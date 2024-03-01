cc1364c20855533622bedb21140366d0566a9d84
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-03-01 20:55:44 +0530

    completing the community page without filter

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,9 +6,25 @@ import { connectToDatabase } from '../mongoose';
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from './shared.types';
 import Question from '../database/question.model';
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
 
 export async function getUserById(params: any) {
   try {
