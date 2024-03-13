ec2a4af14a10c93e5d649e7cb97314c3e47eff76
vishalkumar4491 <vishalkumar.05423@gmail.com>
2024-03-14 00:46:50 +0530

    fixing bug

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -56,7 +56,7 @@ const LeftSidebar = () => {
       </div>
 
       <SignedOut>
-        <div className="flex flex-col gap-3">
+        <div className="mt-5 flex flex-col gap-3">
           <Link href="sign-in">
             <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
               <Image
